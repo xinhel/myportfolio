de92c3afa6d73f5439b32d669b771f9789ed6473
xinhel <xinheapply@gmail.com>
2016-04-12 12:34:42 -0400

    file clean up

--- js/styling.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(document).ready(function(){
-	$('.links').each(function() {
-		console.log($(this).parent().height())
-		$(this).css('margin-top', $(this).parent().height()-$(this).height()-350)
-	});
-  });
-}(jQuery));
